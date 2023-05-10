a16cb10c18e30a784d1ad59b614f16b2f7b2c1de
Bassem Youssef <91248928+BasemYousef@users.noreply.github.com>
2023-05-10 12:16:54 +0300

    Character Movement and Animations (Completed)
    Animations :Roll ,Move by using Nav Mesh

--- FitNot/Assets/_project/Bassem/B_Scripts/CharacterMovement.cs
+++ FitNot/Assets/_project/Bassem/B_Scripts/CharacterMovement.cs
@@ -5,49 +5,63 @@ using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
 [RequireComponent(typeof(NavMeshAgent))]
-public class CharacterMovement : MonoBehaviour,ICharacterMovement
+public class CharacterMovement : MonoBehaviour, ICharacterMovement
 {
+    
+
     #region Serialized Private Variables
     [SerializeField] private LayerMask walkingLayer = new LayerMask();
     [SerializeField] private InputAction movement = new InputAction();
     [SerializeField] private InputAction roll = new InputAction();
-    [SerializeField] private float rollSpeed =10f;
-    [SerializeField] private float rollDuration = 0.5f;
+    [SerializeField] private AnimationCurve dodgeCurve;
+    [SerializeField] private float dodgeForce;
+   
     #endregion
+
     #region Private Variables
     private Camera cam = null;
+    private CharacterController characterController;
     private NavMeshAgent agent = null;
     private Animator anim;
-    private bool isRolling = false;
-
+    private bool isDodging = false;
+    private float dodgeTimer;
+    private float velocityY;
     #endregion
+
     void Start()
     {
         anim = GetComponent<Animator>();
         cam = Camera.main;
         agent = GetComponent<NavMeshAgent>();
+        characterController = GetComponent<CharacterController>();
+        Keyframe lastFrameDodge = dodgeCurve[dodgeCurve.length - 1];
+        dodgeTimer = lastFrameDodge.time;
     }
+
     private void OnEnable()
     {
         movement.Enable();
         roll.Enable();
     }
+
     private void OnDisable()
     {
         movement.Disable();
         roll.Disable();
     }
 
-    private void Update()
+    private void FixedUpdate()
     {
-        HandleInput();
+        if (!isDodging)
+            HandleInput();
+
         UpdateAnimation();
+            OnDodgeRoll();
+    
     }
    
-    
     private void UpdateAnimation()
     {
-        
         if (agent.velocity.magnitude > 0.1f)
         {
             anim.SetBool("Move", true);
@@ -57,24 +71,6 @@ public class CharacterMovement : MonoBehaviour,ICharacterMovement
             anim.SetBool("Move", false);
         }
     }
-   public IEnumerator Roll()
-   {
-
-        Vector3 rollDirection = transform.forward;
-
-        isRolling = true;
-       
-        anim.SetTrigger("Roll");
-        float startTime = Time.time;
-        while (Time.time < startTime + rollDuration)
-        {
-            transform.position += rollDirection * rollSpeed * Time.deltaTime;
-            yield return null;
-        }
-
-        agent.enabled = true;
-        isRolling = false;
-   }
 
     public void MoveTo(Vector3 location)
     {
@@ -83,24 +79,50 @@ public class CharacterMovement : MonoBehaviour,ICharacterMovement
 
     public void HandleInput()
     {
-        if (movement.ReadValue<float>() == 1)
+        if (movement.ReadValue<float>() == 1 )
         {
-            Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+            Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
             anim.SetBool("Move", true);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit, 500, walkingLayer))
             {
                 MoveTo(hit.point);
             }
+            
         }
         else
         {
             anim.SetBool("Move", false);
         }
-        if (roll.triggered && !isRolling)
+    }
+
+    private void OnDodgeRoll()
+    {
+        if (roll.ReadValue<float>() == 1 && !isDodging && anim.GetCurrentAnimatorStateInfo(0).IsName("Run_N") )
         {
-            StartCoroutine(Roll());
+            StartCoroutine(Dodge());
         }
     }
-}
+    IEnumerator Dodge()
+    {
+        anim.SetTrigger("Roll");
+        isDodging = true;
+        float timer = 0;
+        characterController.center = new Vector3(0, 0.3f, 0);
+        characterController.height = 0.3f;
 
+        while (timer < dodgeTimer)
+        {
+            float speed = dodgeCurve.Evaluate(timer);
+            Vector3 dir = (transform.forward * speed) + (Vector3.up * velocityY);
+            characterController.Move(dir* dodgeForce * Time.deltaTime);
+            timer += Time.deltaTime;
+            yield return null;
+        }
+        isDodging = false;
+        characterController.center = new Vector3(0, 0.9f, 0);
+        characterController.height = 1.8f;
+        
+    }
+
+}
